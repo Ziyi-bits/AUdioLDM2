d03bda8799be12c24a3497a7812d4fd23d34473e
Xu, Ziyi <Ziyi.Xu@harman.com>
2026-02-25 11:13:19 +0100

    Update vocoder checkpoint path to absolute path

--- audioldm_train/utilities/model_util.py
+++ audioldm_train/utilities/model_util.py
@@ -264,7 +264,7 @@ def torch_version_orig_mod_remove(state_dict):
 
 
 def get_vocoder(config, device, mel_bins):
-    ROOT = "data/checkpoints"
+    ROOT = "/Volumes/gen_audio_catalog/volumes/ziyi/Checkpoint_AudioLDM2/"
 
     if mel_bins == 64:
         model_path = os.path.join(ROOT, "hifigan_16k_64bins")
